54d5cd99079b482c44c0b580392ef4e6427ab329
AlexGrekovRUCLOUD <grekov@rucloud.host>
2022-10-01 21:32:53 +0300

    Initial commit

---
+++ src/main/java/su/kotindustries/kotnotes/KotnotesApplication.java
@@ -0,0 +1,13 @@
+package su.kotindustries.kotnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotnotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KotnotesApplication.class, args);
+	}
+
+}
