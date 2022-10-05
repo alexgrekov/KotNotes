dc1dbc6ba5637a7d5499c1aaf6be93b25dd65f15
AlexGrekovRUCLOUD <grekov@rucloud.host>
2022-10-05 19:28:54 +0300

    Add simple http authentification with hardcoded creds

---
+++ src/main/java/su/kotindustries/kotnotes/secutityConguration.java
@@ -0,0 +1,43 @@
+package su.kotindustries.kotnotes;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class secutityConguration {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder){
+        UserDetails userAdmin = User.withUsername("admin")
+                .password(passwordEncoder.encode("AdminPassword"))
+                .roles("ADMIN")
+                .build();
+        UserDetails userUser = User.withUsername("user")
+                .password(passwordEncoder.encode("userpass"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(userUser, userAdmin);
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity)throws Exception{
+        httpSecurity.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+        return httpSecurity.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return encoder;
+    }
+}
